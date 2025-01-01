fb5cf47cc5944ffe4bb2432f6364465947e29f58
Kamran <moyunusjmi17@gmail.com>
2025-01-01 20:06:42 +0530

    Resolved data integrity issue and checked db for successful data upload

--- src/controllers/dataControllers.ts
+++ src/controllers/dataControllers.ts
@@ -6,6 +6,7 @@ import Queue from 'bull'
 import {Data} from '../models/dataSchema'
 const queue = new Queue('dataUpload', {redis: {port: 6379, host: '127.0.0.1'}})
 const batch_size = 1000;
+
 type stocksType = {
   date: string, 
   open: number, 
@@ -15,6 +16,58 @@ type stocksType = {
   volume: number, 
   openInt: number
 };
+
+const readStreamFun = async (filePath: string, stocks: stocksType[], res: Response, done: any): Promise<void> => {
+  console.log("filePath 111----", filePath)
+  let readStream = fs.createReadStream(filePath).pipe(csv.parse({delimiter: ",", from_line: 2 }))
+  console.log("filePath 222----", filePath)
+  return new Promise((resolve, reject) => {
+    readStream.on('data', (row) => {
+      //console.log("row data------", row)
+      let stockData: stocksType = {
+        date: row[0],
+        open: parseFloat(row[1]),
+        high: parseFloat(row[2]),
+        low: parseFloat(row[3]),
+        close: parseFloat(row[4]),
+        volume: parseFloat(row[5]),
+        openInt: parseInt(row[6]),
+      }
+      //console.log("stockData====", stockData)
+      stocks.push(stockData)
+      if(stocks.length == batch_size) { 
+        readStream.pause();
+        console.log('Reading paused ! Data is being uploaded to DB !')
+        Data.insertMany(stocks)
+        .then(()=>{
+          console.log('Data inserted successfully ! New data is being loaded !')
+          stocks = []
+          readStream.resume();
+          console.log('Reading resumed !')
+        })
+        .catch((error: Error)=>{
+          console.log(`Some error occured in uploading data to DB ${error}`)
+        })             
+      }
+    })
+    readStream.on('end', async () => {
+      try{
+        if(stocks.length > 0){
+          await Data.insertMany(stocks);
+          console.log('All data inserted successfully !')
+          resolve()
+        }
+      }
+      catch(err: any){
+        return res.status(500).json({message: `Some error occured in inserting last batch of data ${err.message}`})
+      }
+    })
+    readStream.on('error', (error) => {
+      console.log("Error in reading file", error)
+      reject(error)
+    })
+  })
+}
 
 // Function to upload data to the database
 export const uploadData = async(req: Request, res: Response): Promise<any> => {
@@ -29,57 +82,18 @@ export const uploadData = async(req: Request, res: Response): Promise<any> => {
       filePath: dirPath
     }
     queue.add(data)
-    queue.process(async (job, done)=>{
+    queue.process(async (job, done) => {
       const {filePath} = job.data
       const stocksFolder = fs.readdirSync(filePath)
       console.log("stocks file==>", stocksFolder)
       let stocks: stocksType[] = [];
-      let cnt = 0;
-      stocksFolder.map((file) => {
-        cnt++;
-        console.log("cnt-==--=-==", cnt)
-        let filePath = path.join(dirPath, file)
-        let readStream = fs.createReadStream(filePath).pipe(csv.parse({delimiter: ",", from_line: 2 }))
 
-        readStream.on('data', (row) => {
-          console.log("row data------", row)
-          console.log("file ----", file)
-          let stockData: stocksType = {
-            date: row[0],
-            open: parseFloat(row[1]),
-            high: parseFloat(row[2]),
-            low: parseFloat(row[3]),
-            close: parseFloat(row[4]),
-            volume: parseFloat(row[5]),
-            openInt: parseInt(row[6]),
-          }
-          console.log("stockData====", stockData)
-          stocks.push(stockData)
-          if(stocks.length == batch_size) { 
-            // readStream.pause();
-            // Data.insertMany(stocks)
-            // .then(()=>{
-            //   console.log('Data inserted successfully ! New data is being loaded[[[[]]]]')
-            //   stocks = []
-            //   readStream.resume();
-            // })
-            // .catch((error: Error)=>{
-            //   console.log(`Some error occured in uploading data to DB ${error}`)
-            // })             
-          }
-        })
-        readStream.on('end', async () => {
-          try{
-            if(stocks.length > 0){
-              await Data.insertMany(stocks);
-              done();
-            }
-          }
-          catch(err: any){
-            return res.status(500).json({message: err.message})
-          }
-        })
-      })
+      for(let file of stocksFolder){
+        console.log("file name===", file)
+        let filePath = path.join(dirPath, file)
+        await readStreamFun(filePath, stocks, res, done)
+      }
+      done();
     })
   } catch(error: any){
     return res.status(500).json({message: error.message})
